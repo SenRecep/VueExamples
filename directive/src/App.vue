3d4de2df0427144e2366966609009f8311cf2512
Recep Şen <33979963+SenRecep@users.noreply.github.com>
2021-08-01 18:53:13 +0300

    directive

--- directive/src/App.vue
+++ directive/src/App.vue
@@ -1,60 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h3>VueJS Directive'ler</h3>
+        <p v-text="'mail: me@senrecep.com'"></p>
+        <p v-html="'<b>mail</b>: me@senrecep.com'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <h3>Custom Directive'ler</h3>
+        <p v-color.delay="'blue'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo illum
+          veritatis ipsa, omnis dolor incidunt minima asperiores eveniet ducimus nam
+          libero? Quae, omnis sit necessitatibus veniam tempore aperiam! Nostrum!
+        </p>
+        <p v-color:backgroundColor.flash="'yellow'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo illum
+          veritatis ipsa, omnis dolor incidunt minima asperiores eveniet ducimus nam
+          libero? Quae, omnis sit necessitatibus veniam tempore aperiam! Nostrum!
+        </p>
+        <p style="border-style: solid; border-width: thin" v-color:borderColor="'red'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo illum
+          veritatis ipsa, omnis dolor incidunt minima asperiores eveniet ducimus nam
+          libero? Quae, omnis sit necessitatibus veniam tempore aperiam! Nostrum!
+        </p>
+        <button v-custom-on:click="showAlert">Show Alert</button>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+  methods: {
+    showAlert() {
+      alert("Test Basarili");
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
--- directive/src/main.js
+++ directive/src/main.js
@@ -1,6 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('color', {
+  bind(el, binding, vnode) {
+
+    const delay = binding.modifiers['delay'] ? 2000 : 0;
+
+    const work = (color) => {
+      if (binding.arg) {
+        el.style[binding.arg] = color;
+        return;
+      }
+      el.style.color = color;
+    }
+    const flashWork = (color) => {
+      const firstColor = color;
+      const secondColor = 'gray';
+      let currentColor = firstColor;
+      setInterval(() => {
+        currentColor = currentColor == firstColor ? secondColor : firstColor;
+        work(currentColor);
+      }, 1000);
+    };
+
+    const currentWork = binding.modifiers['flash'] ? flashWork : work;
+
+    setTimeout(() => currentWork(binding.value), delay);
+  }
+});
+
+
+Vue.directive('custom-on', {
+  bind(el, binding) {
+    if (!binding.arg || !binding.value) return;
+    el.addEventListener(binding.arg,binding.value);
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
